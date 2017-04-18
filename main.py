e19837e33f3a731e3e5c86a663b0342eea82cf8b
Eivind Teig <eivind.teig@gmail.com>
2017-04-18 14:47:30 +0200

    testing connection to database

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#
+import MySQLdb
+from mysecret import DATABASE_USER, DATABASE_PASS, DATABASE_HOST
+
+db = MySQLdb.connect(host=DATABASE_HOST, user=DATABASE_USER,
+                     passwd=DATABASE_PASS, db="opascree_lan-seats")
+c = db.cursor()
+c.execute("""SELECT * from tickets""")
