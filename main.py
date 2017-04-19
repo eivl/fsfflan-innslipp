9ab7197c914ec0bdba3cc095497a5d963bfd74da
Eivind Teig <eivind.teig@gmail.com>
2017-04-19 08:07:55 +0200

    making some folders

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ db = MySQLdb.connect(host=DATABASE_HOST, user=DATABASE_USER,
                      passwd=DATABASE_PASS, db="opascree_lan-seats")
 c = db.cursor()
 c.execute("""SELECT * from tickets""")
+#
