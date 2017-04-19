6b576edf1174c60b0f6e0f06ee61f181896bae0e
Eivind Teig <eivind.teig@gmail.com>
2017-04-19 14:04:52 +0200

    removed send_from_dir

--- __init__.py
+++ __init__.py
@@ -8,21 +8,22 @@ def homepage():
     return render_template("main.html")
     # return "Hei på deg!"
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.debug = True
+    app.run(host='0.0.0.0')
 
 
-@app.route('/<path:path>')
-def static_file(path):
-    return app.send_static_file(path)
+# @app.route('/<path:path>')
+# def static_file(path):
+#     return app.send_static_file(path)
 
 
-@app.route('/static/<path:path>')
-def send_static(path):
-    return send_from_directory('static', path)
+# @app.route('/static/<path:path>')
+# def send_static(path):
+#     return send_from_directory('static', path)
 
 
-@app.route('/templates/<path:path>')
-def send_templates(path):
-    return send_from_directory('templates', path)
+# @app.route('/templates/<path:path>')
+# def send_templates(path):
+#     return send_from_directory('templates', path)
 
 
