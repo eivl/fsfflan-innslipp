320960d87b84a68a397b055570d54758503254ae
Eivind Teig <eivind.teig@gmail.com>
2017-04-19 11:58:07 +0200

    more tests

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/')
 def homepage():
-    # return render_template("main.html")
-    return "Hei på deg!"
+    return render_template("main.html")
+    # return "Hei på deg!"
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
 
