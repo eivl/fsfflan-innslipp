096d8706179882f4eddfeea6287a54476f3b7f70
Eivind Teig <eivind.teig@gmail.com>
2017-04-19 11:29:01 +0200

    fixing htaccess

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template("main.html")
-
+    # return render_template("main.html")
+    return "Hei på deg!"
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
