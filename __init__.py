bfc30161d3587d6225c9064f3b01a05b5eee5c20
Eivind Teig <eivind.teig@gmail.com>
2017-04-19 11:52:19 +0200

    static url path

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 
 @app.route('/')
@@ -8,4 +8,5 @@ def homepage():
     # return render_template("main.html")
     return "Hei på deg!"
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
+
