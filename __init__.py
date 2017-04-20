f268f1547f90c8b510e4ab9d780a620fedc5b462
Eivind Teig <eivind.teig@gmail.com>
2017-04-20 09:19:32 +0200

    removed jinja2

--- __init__.py
+++ __init__.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, send_from_directory
-import jinja2
-import os
+# import jinja2
+# import os
 
-#tml_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+# tml_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 
 app = Flask(__name__)
-#app = Flask(__name__, template_folder=tml_dir)
-#app.jinja_loader = jinja2.FileSystemLoader('/home/opascree/public_html/reg/fsfflan-innslipp/templates')
+# app = Flask(__name__, template_folder=tml_dir)
+# app.jinja_loader = jinja2.FileSystemLoader('/home/opascree/public_html/reg/fsfflan-innslipp/templates')
 
 
 @app.route('/')
 def homepage():
-    return render_template('main.html')
-    # return "Hei på deg!, Flask virker som det skal.. men mest sannsynlig ikke render_template"
+    #return render_template('main.html')
+    return "Hei på deg!, Flask virker som det skal.. men mest sannsynlig ikke render_template"
 
 
 @app.route('/m2')
